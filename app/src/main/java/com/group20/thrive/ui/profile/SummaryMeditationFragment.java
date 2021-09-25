1ac111d6a7068cd3854bfc9897d2a226334e7224
Trung <trungpro5398@gmail.com>
2021-09-26 04:57:43 +1000

    Update fragment_summary_meditation bar chart

--- app/src/main/java/com/group20/thrive/ui/profile/SummaryMeditationFragment.java
+++ app/src/main/java/com/group20/thrive/ui/profile/SummaryMeditationFragment.java
@@ -2,39 +2,135 @@ package com.group20.thrive.ui.profile;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.group20.thrive.R;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SummaryMeditationFragment extends Fragment {
+
+    private com.group20.thrive.ui.profile.ProfileViewModel ProfileViewModel;
+    private BarChart chart;
+    public TextView noData;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_summary_meditation, container, false);
 
-        GraphView graphView = view.findViewById(R.id.graphView);
+        ProfileViewModel =
+                new ViewModelProvider(this).get(com.group20.thrive.ui.profile.ProfileViewModel.class);
 
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(0,2),
-                new DataPoint(1,5),
-                new DataPoint(2,5),
-                new DataPoint(5,4),
-                new DataPoint(8,7)
+        TextView streakLength = view.findViewById(R.id.streakLen);
+        TextView averageTime = view.findViewById(R.id.averageTime);
+        TextView goalTime = view.findViewById(R.id.goalTime);
+
+        ProfileViewModel.getUser().observe(getActivity(), newData -> {
+            streakLength.setText(String.valueOf(newData.getCurrentStreak()));
+            String goal = newData.getExerciseGoal() + " min";
+            goalTime.setText(goal);
         });
 
-        graphView.setTitle("temp graph view");
-        graphView.setTitleColor(R.color.black);
-        graphView.setTitleTextSize(36);
-        graphView.addSeries(series);
+        ProfileViewModel.getRecordsOfActivityType("meditation").observe(getActivity(), newData -> {
+            int sum = 0;
+            for (int i = 0; i < newData.size(); i++) { sum += newData.get(i).getRecordLength(); }
+            float avgTime = (float) sum / newData.size() / 60;
+            String text = round(avgTime, 1) + " min";
+            averageTime.setText(text);
+        });
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        chart = view.findViewById(R.id.graphView);
+
+        chart.getDescription().setEnabled(false);
+
+        noData = view.findViewById(R.id.noData);
+
+        ProfileViewModel.getRecordsOfActivityTypeInAWeek("meditation").observe(getActivity(), this::setUpGraph);
+    }
+
+    public void setUpGraph(List<Record> recordList) {
+
+        if (recordList.isEmpty()) { noData.setVisibility(View.VISIBLE); }
+
+        List<String> xAxisValues = new ArrayList<>();
+
+        for (int i = recordList.size()-1; i > -1; i--) {
+            xAxisValues.add(recordList.get(i).getRecordTime().substring(0, 5));
+        }
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(xAxisValues));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        ArrayList<BarEntry> values = new ArrayList<>();
+
+        for (int i = 0; i < recordList.size(); i++) {
+            float val = (float) recordList.get(i).getRecordLength() / 60;
+            values.add(new BarEntry(i, val));
+        }
+
+        BarDataSet set1;
+
+        if (chart.getData() != null &&
+                chart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) chart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+
+        } else {
+            set1 = new BarDataSet(values, "minutes");
+
+            int color = ContextCompat.getColor(getActivity(), android.R.color.holo_red_light);
+
+            set1.setColor(color);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(dataSets);
+            data.setValueTextSize(10f);
+            data.setBarWidth(0.9f);
+
+            chart.setData(data);
+            chart.invalidate();
+        }
+    }
+
+    // https://stackoverflow.com/a/35833800/13728158
+    public static float round(float number, int scale) {
+        int pow = 10;
+        for (int i = 1; i < scale; i++)
+            pow *= 10;
+        float tmp = number * pow;
+        return ( (float) ( (int) ((tmp - (int) tmp) >= 0.5f ? tmp + 1 : tmp) ) ) / pow;
     }
 }
